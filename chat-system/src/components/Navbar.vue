bfbc8d65396dffac8dab166eb66900d522a019b7
Arghun-dev <arghun.developer@gmail.com>
2020-03-18 21:05:40 +0330

    Created Navbar and WelcomeScreen components

---
+++ chat-system/src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="navbar">
+        <nav>
+            <ul>
+                <h1>Chat System</h1> 
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            
+        }
+    },
+
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+.navbar {
+    background-color: #1de9b6;
+    padding-top: 1.2rem;
+}
+
+h1 {
+    margin-top: 1rem;
+}
+
+nav {
+    width: 100%;
+}
+
+ul {
+    list-style-type: none; 
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+</style>
---
+++ chat-system/src/components/WelcomeScreen.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="welcome">
+    <div class="box">
+      <h1 class="display-2">Welcome</h1>
+      <form class="mt-3">
+        <div class="form-group">
+          <small for="name">Enter Your Name:</small>
+          <input type="text" class="form-control" id="name" aria-describedby="name">
+          <button type="submit" class="btn button text-uppercase">Enter Chat</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.box {
+  border: 1px solid #ccc;
+  width: 35%;
+  margin-left: 50%;
+  margin-top: 17%;
+  transform: translate(-50%, -50%);
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: .2rem;
+  box-shadow: 1px 2px 2px #ccc;
+}
+
+h1 {
+  color: #1de9b6;
+}
+
+button {
+  background-color: #1de9b6;
+  color: white;
+  margin-top: 8%;
+  margin-left: 25%;
+}
+
+</style>
