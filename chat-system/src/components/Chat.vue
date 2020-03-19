5dc637caa370622a841338c1ea7a358103395230
Arghun-dev <arghun.developer@gmail.com>
2020-03-19 13:15:41 +0330

    Auto scrolling

--- chat-system/src/components/Chat.vue
+++ chat-system/src/components/Chat.vue
@@ -1,17 +1,18 @@
 <template>
     <div class="chat container w-100 ChatContainer">
-        <div class="card border-info mb-3 w-85 MessageCard">
-            <div class="card-header CardHeader" v-for="message in messages" :key='message.id'>
+        <div class="card mb-3 w-85 MessageCard" v-chat-scroll>
+            <div class="CardHeader" v-for="message in messages" :key='message.id'>
                 <div class='messageContent'>
                     <p class="Name">{{ message.name }}:</p>
-                    <p>{{ message.content }}</p>
+                    <p class="w-75">{{ message.content }}</p>
                 </div>
                 <p class="Time">{{ message.timestamp }}</p>
             </div>
-            <div class="card-body text-info">
-                <h5 class="card-title">New message Enter to add</h5>
-                <NewMessage :name="name" />
-            </div>
+            
+        </div>
+        <div class="text-info">
+            <h5 class="card-title">New message Enter to add</h5>
+            <NewMessage :name="name" />
         </div>
     </div>
 </template>
@@ -19,6 +20,7 @@
 <script>
 import NewMessage from './NewMessage'
 import db from '@/firebase/init'
+import moment from 'moment'
 
 export default {
     name: 'Chat',
@@ -43,7 +45,7 @@ export default {
                         id: doc.id,
                         name: doc.data().name,
                         content: doc.data().content,
-                        timestamp: doc.data().timestamp
+                        timestamp: moment(doc.data().timestamp).format('lll')
                     })
                 }
             })
@@ -53,6 +55,26 @@ export default {
 </script>
 
 <style scoped>
+.MessageCard {
+    border: 1px solid #ccc;
+    padding: 1rem;
+    box-shadow: 1px 2px 2px #ccc;
+    max-height: 300px;
+    overflow: auto;
+}
+
+.MessageCard::-webkit-scrollbar {
+    width: 6px;
+}
+
+.MessageCard::-webkit-scrollbar-track {
+    background-color: #ddd;
+}
+
+.MessageCard::-webkit-scrollbar-thumb {
+    background-color: #aaa;
+}
+
 .ChatContainer {
     display: flex;
     flex-direction: column;
@@ -62,14 +84,17 @@ export default {
 .Name {
     color: #1de9b6;
     margin-right: 2rem;
+    font-size: 1.2rem;
 }
 
 .messageContent {
     display: flex;
+    font-size: 1.2rem;
 }
 
 .Time {
     color: #ccc;
     margin-top: -1rem;
+    font-size: .8rem;
 }
 </style>
