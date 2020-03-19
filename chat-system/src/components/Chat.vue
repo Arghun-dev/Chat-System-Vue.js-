16ab462e6fe58131cb1f141d59814134adbd3ef4
Arghun-dev <arghun.developer@gmail.com>
2020-03-19 10:38:51 +0330

    Created Chat Window

--- chat-system/src/components/Chat.vue
+++ chat-system/src/components/Chat.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="chat container">
-        <h2>Chat {{ name }}</h2>
+    <div class="chat container w-100 ChatContainer">
+        <div class="card border-info mb-3 w-85 MessageCard">
+            <div class="card-header">time</div>
+            <div class="card-body text-info">
+                <h5 class="card-title">{{ name }}</h5>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+        </div>
     </div>
 </template>
 
@@ -17,5 +23,9 @@ export default {
 </script>
 
 <style scoped>
-
+.ChatContainer {
+    display: flex;
+    flex-direction: column;
+    padding-top: 5%;
+}
 </style>
