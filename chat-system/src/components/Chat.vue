e694fabfff346af4c13e20c6f9ad8d7a896a7d33
Arghun-dev <arghun.developer@gmail.com>
2020-03-19 12:49:35 +0330

    Real Time Events (Event Listeners)

--- chat-system/src/components/Chat.vue
+++ chat-system/src/components/Chat.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="chat container w-100 ChatContainer">
         <div class="card border-info mb-3 w-85 MessageCard">
-            <div class="card-header">time</div>
+            <div class="card-header CardHeader" v-for="message in messages" :key='message.id'>
+                <div class='messageContent'>
+                    <p class="Name">{{ message.name }}:</p>
+                    <p>{{ message.content }}</p>
+                </div>
+                <p class="Time">{{ message.timestamp }}</p>
+            </div>
             <div class="card-body text-info">
-                <h5 class="card-title">{{ name }}</h5>
+                <h5 class="card-title">New message Enter to add</h5>
                 <NewMessage :name="name" />
             </div>
         </div>
@@ -12,6 +18,7 @@
 
 <script>
 import NewMessage from './NewMessage'
+import db from '@/firebase/init'
 
 export default {
     name: 'Chat',
@@ -21,8 +28,26 @@ export default {
     },
     data() {
         return {
-
+            messages: []
         }
+    },
+
+    created() {
+        let ref = db.collection('messages').orderBy('timestamp')
+
+        ref.onSnapshot(snapshot => {
+            snapshot.docChanges().forEach(change => {
+                if(change.type == 'added'){
+                    let doc = change.doc
+                    this.messages.push({
+                        id: doc.id,
+                        name: doc.data().name,
+                        content: doc.data().content,
+                        timestamp: doc.data().timestamp
+                    })
+                }
+            })
+        })
     }
 }
 </script>
@@ -32,5 +57,19 @@ export default {
     display: flex;
     flex-direction: column;
     padding-top: 5%;
+}
+
+.Name {
+    color: #1de9b6;
+    margin-right: 2rem;
+}
+
+.messageContent {
+    display: flex;
+}
+
+.Time {
+    color: #ccc;
+    margin-top: -1rem;
 }
 </style>
