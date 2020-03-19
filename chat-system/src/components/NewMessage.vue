a74c2b88accf37941bfc4f6ac2a084215b556bbc
Arghun-dev <arghun.developer@gmail.com>
2020-03-19 12:17:38 +0330

    Adding messages to the firestore

--- chat-system/src/components/NewMessage.vue
+++ chat-system/src/components/NewMessage.vue
@@ -1,19 +1,40 @@
 <template>
     <div class="new-message">
-        <form>
+        <form @submit.prevent="addNewMessage">
             <div class="form-group">
                 <input type="text" class="form-control" v-model="message">
+                <p v-if="feedback" class="text-danger mt-1 ml-1">{{ feedback }}</p>
             </div>
         </form>
     </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
     name: 'NewMessage',
+    props: ['name'],
     data() {
         return {
-            message: null
+            message: null,
+            feedback: null
+        }
+    },
+
+    methods: {
+        addNewMessage(){
+            if(this.message){
+                db.collection('messages').add({
+                    content: this.message,
+                    name: this.name,
+                    timestamp: Date.now()
+                }).catch((err) => console.log(err))
+                this.message = null
+                this.feedback = null
+            } else {
+                this.feedback = 'You have to enter some text'
+            }
         }
     }
 }
